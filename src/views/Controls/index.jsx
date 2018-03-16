f7095ec6afa68f17045fcc0c8913bb44bd3fc6c7
Mick Horler <m.p.horler@gmail.com>
2018-03-16 11:50:31 +0000

    Removed duplication around the player columns in the controls jsx

--- src/views/Controls/index.jsx
+++ src/views/Controls/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { filter } from 'ramda';
 import { connect } from 'react-redux';
 import actions from '../../actions';
 import styles from './index.css';
@@ -12,36 +13,41 @@ const Control = ({ name, onClick }) => (
     <Icon name={name} />
   </button>);
 
+/**
+  the .join is a bit grim, should probably have used classNames or write a helper
+  function to at least hide it...
+*/
 const PreviousGame = ({ result }) => (
   <li className={[result.won && styles.won, styles.result].join(' ')}>
     <Icon name={result.throw} />
   </li>
 );
 
+const Player = ({ games, player, scores }) => (
+  <div className={[styles.side, styles.board].join(' ')}>
+    <h2 className={styles['player-title']}>{player} ({scores[player]})</h2>
+    <ul className={styles.results}>
+      {
+        games.map(game => {
+          const thisPlayerSelection = filter(round => round.team === player, game);
+          return (
+            <PreviousGame
+              key={thisPlayerSelection[0].id}
+              result={thisPlayerSelection[0]}
+            />
+          );
+        })
+      }
+    </ul>
+  </div>
+);
+
 const Controls = ({ games, hands, scores, throwHand }) => [
-  <ul>
-    <li>
-      {hands.map(hand => (
-        <Control name={hand.type} onClick={throwHand} />
-      ))}
-    </li>
-  </ul>,
-  <div className={[styles.side, styles.board, styles['team-one']].join(' ')}>
-    <h2 className={styles['player-title']}>Player ({scores.player})</h2>
-    <ul className={styles.results}>
-      {games.map(game => (
-        <PreviousGame result={game[0]} />
-      ))}
-    </ul>
-  </div>,
-  <div className={[styles.side, styles.board, styles['team-two']].join(' ')}>
-    <h2 className={styles['player-title']}>CPU ({scores.cpu})</h2>
-    <ul className={styles.results}>
-      {games.map(game => (
-        <PreviousGame result={game[1]} />
-      ))}
-    </ul>
-  </div>,
+  hands.map(hand =>
+    <Control key={hand.id} name={hand.type} onClick={throwHand} />
+  ),
+  <Player games={games} player="player" scores={scores} />,
+  <Player games={games} player="cpu" scores={scores} />,
 ];
 
 export default connect(
