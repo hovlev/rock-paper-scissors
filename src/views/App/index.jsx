48e09c1f547ad19fdbfa818e7913c3b6ea471198
Mick Horler <m.p.horler@gmail.com>
2018-03-16 02:50:20 +0000

    Added additional title to app jsx

--- src/views/App/index.jsx
+++ src/views/App/index.jsx
@@ -5,6 +5,7 @@ import Controls from '../Controls';
 const App = () => (
   <section>
     <h1 className={styles.title}>Rock, paper, scissors</h1>
+    <h2 className={styles.subtitle}>Choose your weapon</h2>
     <Controls />
   </section>
 );
