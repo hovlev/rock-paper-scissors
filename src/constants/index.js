3979cb554d03a39c75b1fb4c04eec7fe41d8c3a8
Mick Horler <m.p.horler@gmail.com>
2018-03-16 11:50:14 +0000

    Defaults -> Constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,13 +2,16 @@ export default [
   {
     type: 'rock',
     beats: 'scissors',
+    id: 0,
   },
   {
     type: 'paper',
     beats: 'rock',
+    id: 1,
   },
   {
     type: 'scissors',
     beats: 'paper',
+    id: 2,
   },
 ];
