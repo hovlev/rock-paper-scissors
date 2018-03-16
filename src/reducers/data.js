86c6960ff696bc5c8e00d6f47dd6f6584549fb90
Mick Horler <m.p.horler@gmail.com>
2018-03-16 02:50:56 +0000

    added score counting and game history logic to reducer

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,19 +1,41 @@
 import hands from '../defaults';
-import { append, assoc, prop } from 'ramda';
+import { append, assoc, filter, flatten, head, length, map, merge, prop, reduce, last } from 'ramda';
 
 const init = {
   hands,
   games: [],
+  scores: {
+    player: 0,
+    cpu: 0
+  },
 };
 
-const throwHand = (state, payload) => {
-  return assoc('games', append(payload, prop('games', state)), state);
+const appendGame = (state, payload) => {
+  const games = prop('games', state);
+  return append(payload, games);
+};
+
+const countScore = games =>
+  length(filter(game => game.won, games));
+
+const getScores = games => {
+  const player = countScore(map(game => head(game), games));
+  const cpu = countScore(map(game => last(game), games));
+  return {
+    player,
+    cpu
+  }
 };
 
 export default (state = init, { payload, type }) => {
   switch (type) {
     case 'THROW_COMPLETED':
-      return throwHand(state, payload);
+      const updatedGames = appendGame(state, payload);
+      const updatedScores = getScores(updatedGames);
+      return merge(state, {
+        games: updatedGames,
+        scores: updatedScores,
+      });
     case 'SWITCH_MODE':
       return state;
     default: {
