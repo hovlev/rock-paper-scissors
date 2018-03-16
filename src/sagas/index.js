e6678319240fae9d3c6f5aa528ef099e7703a621
Mick Horler <m.p.horler@gmail.com>
2018-03-16 11:51:21 +0000

    Minor tweaks to saga, comments and more functional

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,29 +1,39 @@
-import { assoc, equals, filter, find, map, not, prop, propEq } from 'ramda';
-
+import { equals, filter, find, head, map, merge, not, prop, propEq, length } from 'ramda';
 import { takeLatest, put } from 'redux-saga/effects';
 import actions from '../actions';
-import hands from '../defaults';
+import hands from '../constants';
 
+/**
+  This is the CPU's hand selection
+*/
 const getRandomHandIndex = () => Math.floor(Math.random() * hands.length);
 
+/**
+  I've simulated a server-like flow here, as obviously the server would contain
+  the logic around deciding the winner
+*/
 const setWinner = results =>
   map(result => {
+    const team = prop('team', result);
     const otherPlayer = filter(filteredResult =>
-      not(equals(prop('team', result), prop('team', filteredResult))), results);
-    const otherThrow = otherPlayer[0].throw;
-    const currentHand = find(propEq('type', prop('throw', result)))(hands);
-    return assoc('won', equals(currentHand.beats, otherThrow), result);
+      not(equals(team, prop('team', filteredResult))), results);
+    const otherThrow = prop('throw', head(otherPlayer));
+    const currentThrow = find(propEq('type', prop('throw', result)))(hands);
+    return merge(result, {
+      id: `${team}-${new Date().getTime()}`,
+      won: equals(currentThrow.beats, otherThrow),
+    });
   }, results);
 
 const calculateResult = payload => {
   const results = [
     {
       team: 'player',
-      throw: payload
+      throw: payload,
     },
     {
       team: 'cpu',
-      throw: hands[getRandomHandIndex()].type
+      throw: hands[getRandomHandIndex()].type,
     },
   ];
   return setWinner(results);
